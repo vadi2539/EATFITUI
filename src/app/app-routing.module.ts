4cb950e52a594e0678cec3eb1fa6db688ff987c8
Singaravelan Narayanan <101590049+vadi2539@users.noreply.github.com>
2022-12-01 13:54:03 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './Component/home/home.component';
+
+import { CartComponent } from './Component/cart/cart.component';
+import { FoodComponent } from './Component/food/food.component';
+import { LoginComponent } from './Component/login/login.component';
+import { LayoutComponent } from './Component/layout/layout.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: LayoutComponent, children: [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component:HomeComponent},
+    {path:'breakfast', component:FoodComponent},
+    {path:'login',component:LoginComponent},
+    {path:'food',component:FoodComponent},
+    {path:'cart',component:CartComponent},
+    
+
+
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './Component/layout/layout.component';
+import { HomeComponent } from './Component/home/home.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatDialogModule} from '@angular/material/dialog';
+import { LoginComponent } from './Component/login/login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FoodComponent } from './Component/food/food.component';
+import { Header1Component } from './Component/header1/header1.component';
+import { LocationComponent } from './Component/location/location.component';
+import { CartComponent } from './Component/cart/cart.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    FoodComponent,
+    Header1Component,
+    LocationComponent,
+    CartComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatTabsModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+    
+    
+  
+    
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
